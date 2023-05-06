b08fee1d03d7ca6e7d96908c6274f272b2fbfc19
Aryana <112254501+aryana64@users.noreply.github.com>
2023-05-06 10:05:13 +0700

    Update alignment.sh

--- alignment.sh
+++ alignment.sh
@@ -11,4 +11,4 @@ conda install bwa
 conda install -c bioconda samtools
 REF=/beegfs/data/hpcws/ws1/anudarieva-exocmar/hg38/hg38.fa
 bwa index -p/beegfs/data/hpcws/ws1/anudarieva-exocmar/scripts/projects/ref_index -a bwtsw $REF   #indexing the reference genome
-bwa mem -t 20 /beegfs/data/hpcws/ws1/anudarieva-exocmar/scripts/projects/ref_index /beegfs/data/hpcws/ws1/anudarieva-exocmar/N32/N32_FKDL220180557-1A_HJCT5DSX3_L1_1.fq.gz /beegfs/data/hpcws/ws1/anudarieva-exocmar/N32/N32_FKDL220180557-1A_HJCT5DSX3_L1_2.fq.gz | samtools sort -o /beegfs/data/hpcws/ws1/anudarieva-exocmar/scripts/projects/output.sorted.bam #alignment itself and preparation to work with IGV
+bwa mem -t 20 /beegfs/data/hpcws/ws1/anudarieva-exocmar/scripts/projects/ref_index /beegfs/data/hpcws/ws1/anudarieva-exocmar/N32/N32_FKDL220180557-1A_HJCT5DSX3_L1_1.fq.gz /beegfs/data/hpcws/ws1/anudarieva-exocmar/N32/N32_FKDL220180557-1A_HJCT5DSX3_L1_2.fq.gz | samtools sort -o /beegfs/data/hpcws/ws1/anudarieva-exocmar/scripts/projects/output.sorted.bam #alignment itself and sorting in a 'genome order'
